88ab52c9813f5a219e9e7cbe06a579cc62bcd5e0
MVRTIN7 <martsanhuezac@gmail.com>
2026-01-17 20:08:14 -0300

    Fix MercadoPago env vars

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -4,7 +4,10 @@ import os
 # MERCADO PAGO
 # =====================
 MP_PUBLIC_KEY = os.getenv("APP_USR-dd17344d-a0e7-4d80-978f-d7a696220a31")
-MP_ACCESS_TOKEN = os.getenv("APP_USR-4456530589942283-011417-56c9693ff1ffd2b4816075f30e70744c-643401778")
+MP_ACCESS_TOKEN = os.getenv("MP_ACCESS_TOKEN")
+
+if not MP_ACCESS_TOKEN:
+    raise ValueError("❌ MP_ACCESS_TOKEN no está definido en las variables de entorno")
 
 # =====================
 # PLANES DE CRÉDITOS
